a46c6707e5b01860261b61ff31311bb10cd5854a
Andrii Ukrainets <alphamaster85@gmail.com>
2018-10-10 01:30:15 +0300

    Homework 5.5

--- CalcPRO.js
+++ CalcPRO.js
@@ -1,19 +1,35 @@
-var readButton = document.getElementById("readNumbers");
 var op;
 var minIn;
 var maxIn;
 var res;
 var counter = [0, 0];
 
+var readButton = document.getElementById("readNumbers");
 readButton.onclick = function () {
     minIn = random(document.getElementById("min").value, document.getElementById("max").value);
     maxIn = random(document.getElementById("min").value, document.getElementById("max").value);
     op = document.getElementById("opSelect").options[document.getElementById("opSelect").selectedIndex].value;
 
     document.getElementById("In").value = minIn + " " + op + " " + maxIn + " = ?";
+}
 
+var checkButton = document.getElementById("checkResult");
+checkButton.onclick = function () {
     res = operation(minIn, maxIn, op);
+    document.getElementById("result").value = res;
     console.log(res);
+
+    if (document.getElementById("prognosRes").value == res) {
+        var congrats = "ВІРНО";
+        counter[0]++;
+    } else {
+        var congrats = "HE ВІРНО";
+        counter[1]++;
+    }
+
+    document.getElementById("resCongrats").value = congrats;
+    console.log(congrats);
+    console.log(counter[0] + " / " + counter[1]);
 }
 
 function operation(x, y, op) {
@@ -35,27 +51,9 @@ function operation(x, y, op) {
             result = x / y;
         } 
     }
-    console.log("результат ", op, " становить ", result);
     return result;
 }
 
 function random(min, max){
     return Math.floor(Math.random() * (max - min) + +min);
-}
-
-var checkButton = document.getElementById("checkResult");
-checkButton.onclick = function () {
-    document.getElementById("result").value = res;
-    console.log(res);
-
-    if (document.getElementById("prognosRes").value == res) {
-        var congrats = "ВІРНО";
-        counter[0]++;
-    } else {
-        var congrats = "HE ВІРНО";
-        counter[1]++;
-    }
-    document.getElementById("resCongrats").value = congrats;
-    console.log(congrats);
-    console.log(counter[0] + " / " + counter[1]);
 }
