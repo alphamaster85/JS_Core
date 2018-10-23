c87fa35c6acfea336d13c03a41b3251d07fa9548
Andrii Ukrainets <alphamaster85@gmail.com>
2018-10-23 07:53:48 +0300

    Homework 7.2 - 2

--- Lection 7.1/task1-3.js
+++ Lection 7.1/task1-3.js
@@ -1,13 +1,25 @@
 var TV = {
     currentChannel : 1,
     nextChannel() {
-        this.currentChannel++;
+        if (this.currentChannel == 32) {
+			this.currentChannel = 1;
+		} else {
+            this.currentChannel++;
+        }
     },
     previousChannel() {
-        this.currentChannel--;
+        if (this.currentChannel == 1) {
+			this.currentChannel = 32;
+        } else {
+            this.currentChannel--;
+        }
     },
     setChannel(number) {
-        this.currentChannel = number;
+        if (number >= 1 && number <= 32) {
+            this.currentChannel = number;
+        } else {
+            console.log("Number of channel is not correct");
+        }
     },
     printChannel() {
         console.log("channel", this.currentChannel);
@@ -17,71 +29,84 @@ console.log(TV);
 
 var MP3 = {
     playList : [song1, song2, song3],
+    i: 0,
     playSong : playList[i],
     nextButton() {
-        this.playSong[i++];
+        if (this.playSong == this.playList[this.playList.length-1]) {
+			this.playSong = playSong[0];
+		} else {
+            this.playSong[++i];
+        }
     },
     prevButton() {
-        this.playSong[i--];
+        if (this.playSong == this.playList[0]) {
+			this.playSong = this.playList[this.playList.length-1];
+		} else {
+            this.playSong[--i];
+        }
     },
-    playButton(song) {
-        this.playSong[song];
+    playButton(songNumber) {
+        if (songNumber >= 0 && songNumber <= this.playList.length-1) {
+            this.playSong = this.playList[songNumber];
+        } else {
+            console.log("Number of song is not correct");
+        }
     },
     stopButton() {
-        this.playSong[i] = NULL;
+        this.playSong = NULL;
     }
 }
 
 class TV {
-    constructor (name_TV) {
-        this.TV = name_TV;
-    };
-    getTV () {
-        return this.TV;
-    };
-    setTV (name_TV) {
-        this.TV = name_TV;
-    };
-    currentChannel = 1;
-    nextChannel() {
-        this.currentChannel++;
-        return this.currentChannel;
-    };
-    previousChannel() {
-        this.currentChannel--;
-        return this.currentChannel;
-    };
-    setChannel(number) {
-        this.currentChannel = number;    
-        return this.currentChannel;
-    };
+    // constructor (name_TV) {
+    //     this.TV = name_TV;
+    // };
+    // getTV () {
+    //     return this.TV;
+    // };
+    // setTV (name_TV) {
+    //     this.TV = name_TV;
+    // };
+    // currentChannel = 1;
+    // nextChannel() {
+    //     this.currentChannel++;
+    //     return this.currentChannel;
+    // };
+    // previousChannel() {
+    //     this.currentChannel--;
+    //     return this.currentChannel;
+    // };
+    // setChannel(number) {
+    //     this.currentChannel = number;    
+    //     return this.currentChannel;
+    // };
 }
 
 class MP3 {
-    constructor (name_MP3) {
-        this.MP3 = name_MP3;
-    };
-    getMP3 () {
-        return this.MP3;
-    };
-    setMP3 (name_MP3) {
-        this.MP3 = name_MP3;
-    };
-    currentSong = "song.mp3";
-    nextSong() {
-        this.currentSong++;
-        return this.currentSong;
-    };
-    previousSong() {
-        this.currentSong--;
-        return this.currentSong;
-    };
-    playSong(song) {
-        this.currentSong = "song";    
-        return this.currentSong;
-    };
-    stopSong() {
-        this.currentSong = "";
-        return this.currentSong;
-    };
+    // constructor (name_MP3) {
+    //     this.MP3 = name_MP3;
+    // };
+    // getMP3 () {
+    //     return this.MP3;
+    // };
+    // setMP3 (name_MP3) {
+    //     this.MP3 = name_MP3;
+    // };
+    // currentSong = "song.mp3";
+    // nextSong() {
+    //     this.currentSong++;
+    //     return this.currentSong;
+    // };
+    // previousSong() {
+    //     this.currentSong--;
+    //     return this.currentSong;
+    // };
+    // playSong(song) {
+    //     this.currentSong = "song";    
+    //     return this.currentSong;
+    // };
+    // stopSong() {
+    //     this.currentSong = "";
+    //     return this.currentSong;
+    // };
 }
